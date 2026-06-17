9bcdad0112a6e67d92213ea0af8fb971434b7447
Troy200 <kuriyanthomas27@gmail.com>
2026-06-17 17:58:16 +0200

    Add files via upload

---
+++ Degree converter/converter.py
@@ -0,0 +1,12 @@
+from tkinter import * 
+
+root=Tk()
+root.geometry("500x500")
+
+
+
+
+titlelable=Label(root, text="Celcius -> Farenheit", )
+titlelable.grid(row=0,column=0)
+
+Celinput=Lable(root, text="Enter degrees =")
